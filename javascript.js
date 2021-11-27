d74871ff255ca44cccabc2f9e5b6674164d47dbc
Clyde <noneclyde@gmail.com>
2021-11-27 18:48:21 +0200

    Add function clearBoard()

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 function createGrid(dimensions) {
-    for (let i = 1; i <= dimensions; i++) {
+    for (let i = 0; i < dimensions; i++) {
         const col = document.createElement('div');
 
         col.style.cssText = 'display: flex; flex: 1 0 0';
 
         container.appendChild(col);
 
-        for (let n = 1; n <= dimensions; n++) {
+        for (let n = 0; n < dimensions; n++) {
             const item = document.createElement('div');
 
             item.classList.add('items');
@@ -17,18 +17,25 @@ function createGrid(dimensions) {
     }
 }
 
+function clearBoard() {
+    items.forEach(item => {
+        item.style.background = 'white';
+    })
+}
+
 function randomColor() {
     return '#' + (Math.floor(Math.random()*16777215).toString(16));
 }
 
 const container = document.querySelector('.container');
 
-createGrid(5);
+createGrid(15);
 
 const items = document.querySelectorAll('.items');
 
 items.forEach(item => {
-    item.addEventListener('mouseover', function(e) {
-        e.target.style.background = randomColor();
+    item.addEventListener('mouseover', () => {
+        item.style.background = randomColor();
     });
 });
+
